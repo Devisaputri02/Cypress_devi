b72188e4cff7b43773a52c4ef1e7364efefaa805
Devisaputri02 <saputrid488@gmail.com>
2025-08-26 00:37:03 +0700

    Quiz 3 - Test Scenario Login OrangeHRM

--- cypress/e2e/3-test/login.cy.js
+++ cypress/e2e/3-test/login.cy.js
@@ -1,26 +1,83 @@
-/*
-describe ('Scenario Login', () => {
-    it('TC01-Valid Login', () => {
-        cy.visit('https://www.saucedemo.com/')
-        cy.get('#user-name').type('standar_user')
-    })
-})
+describe('Login Feature - OrangeHRM', () => {
+  const baseUrl = 'https://opensource-demo.orangehrmlive.com/web/index.php/auth/login';
 
+  beforeEach(() => {
+    cy.visit(baseUrl);
+  });
 
-describe ('Scenario forgot password', () => {
-    it('TC02-Login dengan credential valid', () => {
-        cy.visit('https://www.saucedemo.com/v1/index.html')
-        cy.get('[data-test="password"]').type('testing')
-        cy.get('#login-button').click()
-    })
-})
- */
-describe ('Scenario verifikasi fungsi login', () => {
-    it('TC03-Login dengan username dan password valid', () => {
-        cy.visit('https://www.saucedemo.com/v1/index.html')
-        cy.get('#user-name').type('standard_user').should('have.value','standard_user')
-        cy.get('#password').type('secret_sauce').should('be.visible')
-        cy.get('.btn_action').click()
-        cy.url().should('include', 'inventory')
-    })
-}) 
+  it('TC_001: Login dengan username dan password valid', () => {
+    cy.get("input[name='username']").type('Admin');
+    cy.get("input[name='password']").type('admin123');
+    cy.get("button[type='submit']").click();
+    cy.url().should('include', '/dashboard');
+    cy.get('h6.oxd-text--h6.oxd-topbar-header-breadcrumb-module', { timeout: 10000 })
+    .should('have.text', 'Dashboard');
+
+  });
+
+  it('TC_002: Login dengan username dan password salah', () => {
+    cy.get("input[name='username']").type('Adamin');
+    cy.get("input[name='password']").type('admin124');
+    cy.get("button[type='submit']").click();
+    cy.get('.oxd-alert-content-text').should('contain.text', 'Invalid credentials');
+  });
+
+  it('TC_003: Login dengan username salah', () => {
+    cy.get("input[name='username']").type('Adamin');
+    cy.get("input[name='password']").type('admin123');
+    cy.get("button[type='submit']").click();
+    cy.get('.oxd-alert-content-text').should('contain.text', 'Invalid credentials');
+  });
+
+  it('TC_004: Login dengan password salah', () => {
+    cy.get("input[name='username']").type('Admin');
+    cy.get("input[name='password']").type('admin124');
+    cy.get("button[type='submit']").click();
+    cy.get('.oxd-alert-content-text').should('contain.text', 'Invalid credentials');
+  });
+
+  it('TC_005: Login tanpa mengisi username dan password', () => {
+    cy.get("button[type='submit']").click();
+    cy.get('.oxd-input-field-error-message').should('have.length', 2);
+    cy.get('.oxd-input-field-error-message').each(($el) => {
+      cy.wrap($el).should('contain.text', 'Required');
+    });
+  });
+
+  it('TC_006: Login tanpa mengisi username', () => {
+    cy.get("input[name='password']").type('admin123');
+    cy.get("button[type='submit']").click();
+    cy.get('.oxd-input-field-error-message').first().should('contain.text', 'Required');
+  });
+
+  it('TC_007: Login tanpa mengisi password', () => {
+    cy.get("input[name='username']").type('Admin');
+    cy.get("button[type='submit']").click();
+    cy.get('.oxd-input-field-error-message').first().should('contain.text', 'Required');
+  });
+
+  it('TC_008: Login dengan username huruf kecil semua', () => {
+    cy.get("input[name='username']").type('admin');
+    cy.get("input[name='password']").type('admin123');
+    cy.get("button[type='submit']").click();
+    cy.url().should('include', '/dashboard');
+    cy.contains('Time at Work').should('be.visible');
+  });
+
+  it('TC_009: Reset password dengan username valid', () => {
+    cy.contains('Forgot your password?').click();
+    cy.get("input[placeholder='Username']").type('Admin');
+    cy.get("button[type='submit']").click();
+    cy.get('.orangehrm-card-container')
+      .should('contain.text', 'Reset Password link sent successfully');
+  });
+
+  it('TC_010: Pengguna bisa klik tombol Cancel di halaman reset password', () => {
+  cy.contains('Forgot your password?').click();
+  cy.url().should('include', '/requestPasswordResetCode');
+  cy.get('button[type="button"]').contains('Cancel').click();
+  cy.url().should('include', '/auth/login');
+  cy.get('.oxd-alert-content-text').should('not.exist');
+});
+
+});
